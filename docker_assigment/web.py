4da5109d77578e3968328ce66b2018c59c0f8034
Jeff Pavia <jpavia@comcast.net>
2022-11-21 16:31:58 -0500

    initial commit

---
+++ docker_assigment/web.py
@@ -0,0 +1,64 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import sys
+import json
+import os
+
+# Set environment variables
+#os.environ['SYMBOL'] = 'MSFT'
+#os.environ['NDAYS'] = '7'
+
+# Get environment variables
+SYMBOL = os.getenv('SYMBOL')
+NDAYS = int(os.getenv('NDAYS'))
+
+print(f"NDAYS is {NDAYS} type {type(NDAYS)}")
+print(f"SYMBOL is {SYMBOL}")
+hostName = "0.0.0.0"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        from urllib.request import urlopen
+        url = "https://www.alphavantage.co/query?apikey=C227WD9W3LUVKVV9&function=TIME_SERIES_DAILY_ADJUSTED&symbol="+SYMBOL
+        response = urlopen(url)
+        stock = (json.loads(response.read()))
+        stock=(stock['Time Series (Daily)'])
+        day=(stock["2022-11-18"])
+        n=0
+        sum=0.0
+        cprice=[]
+        for i in stock:
+            n=n + 1
+            day=(stock[i])
+            price=day["4. close"]
+            price=float(price)
+            cprice.append(price)
+            sum=sum + price
+            if n==NDAYS:
+                break
+        avg=sum/int(NDAYS) 
+     
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Stock Symbol: %s</p>" % SYMBOL, "utf-8"))
+        self.wfile.write(bytes("<p>Average Closing Price for last %s days</p>" % NDAYS, "utf-8"))
+        self.wfile.write(bytes("<p>Daily Prices: %s</p>" % cprice, "utf-8"))
+        self.wfile.write(bytes("<p>Average Closing Price: %s</p>" % avg, "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")

61e319b4c785504c11140230373601f4ba4ad325
Jeff Pavia <jpavia@comcast.net>
2022-11-22 17:33:24 -0500

    added secret for apikey

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@
  kubectl apply -f k8s/deployment.yaml
  kubectl apply -f k8s/service.yaml
  kubectl apply -f k8s/config.yaml
+ kubectl apply -f k8s/secret.yaml
  sleep 10
  #kubectl port-forward service/rock 8080:8080 &
